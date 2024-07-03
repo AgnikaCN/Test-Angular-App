f18746ee7060e9e4aeba49843a7f18e77543c1cd
AgnikaCN <agnika@capitalnumbers.com>
2024-07-03 14:26:11 +0530

    multi-content-projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MultiContentProjectionComponent } from './multi-content-projection/multi-content-projection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MultiContentProjectionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'test-app';
---
+++ src/app/multi-content-projection/multi-content-projection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-content-projection',
+  standalone: true,
+  imports: [],
+  templateUrl: './multi-content-projection.component.html',
+  styleUrl: './multi-content-projection.component.scss'
+})
+export class MultiContentProjectionComponent {
+
+}
