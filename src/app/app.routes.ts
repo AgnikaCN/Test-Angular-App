08ef2e8b0ed237a24c0c32a3e97ab8bd57d574a0
AgnikaCN <agnika@capitalnumbers.com>
2024-07-08 14:26:02 +0530

    dropdown and click me button working - parent-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { MultiContentProjectionComponent } from './multi-content-projection/multi-content-projection.component';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ParentComponent } from './parent/parent.component';
 
 export const routes: Routes = [
   // { path: '', component: AppComponent },
@@ -12,4 +13,5 @@ export const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent,
   },
+  { path: 'parent', component: ParentComponent },
 ];
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.scss'
+  styleUrl: './parent.component.scss',
 })
 export class ParentComponent {
+  isDropdownOpen: boolean = false;
 
+  toggleDropdown() {
+    console.log('object');
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  openModal() {
+    console.log('open modal');
+  }
+
+  onButtonClick() {
+    window.alert('Clicked');
+  }
 }
